3315029aa9729b9504f140c67e6c36e7c50f213f
jadhavmrinalini98 <114711523+jadhavmrinalini98@users.noreply.github.com>
2023-02-01 19:34:33 -0500

    code changes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+const methodOverride = require('method-override');
+
 
 app.use(bodyParser.json());
 
@@ -10,6 +12,11 @@ app.get('/healthz',function(req, res) {
     res.status(200).send(); 
 });
 
+app.use(methodOverride());
+app.use((err, req, res, next) => {
+
+  return res.status(400).json({message: "Bad Request"});
+});
 app.use('/v1/user',userRoutes);
 
 module.exports = app;
